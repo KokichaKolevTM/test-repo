36ad6a7b253abf97a9a287f6571e68e196b5294e
KokichaKolevTM <86848204+KokichaKolevTM@users.noreply.github.com>
2026-02-25 12:36:48 +0200

    Add route to create a user

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
+app.get("/create/user", (req, res) => {
+  res.send({ res: "created user!"});
+});
+
 app.listen(8080, () => {
   console.log("Running on port 8080...");
 });
